a8f5cb4eb6c325454ae970499adf93133d6f9fe0
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-01-01 13:30:19 +0530

    add

---
+++ src/routes/movieRoutes.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/hello", (req, res) => {
+  res.json({
+    messages: "heloo from movies",
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default router;
